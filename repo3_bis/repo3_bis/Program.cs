a3935c15f730f017a6813ad7939d5603cb8710c2
Rachida <rachida_hmidouche@yahoo.ca>
2018-07-20 14:19:40 +0200

    Revue

--- repo3_bis/repo3_bis/Program.cs
+++ repo3_bis/repo3_bis/Program.cs
@@ -11,7 +11,7 @@ namespace repo3_bis
         static void Main(string[] args)
         {
             Console.WriteLine("Repo3");
-            Console.WriteLine("version 3");
+            Console.WriteLine("version 4");
             Console.WriteLine("by kim");
             Console.WriteLine("Modified by Rachida");
             Console.WriteLine("Hehehe je suis là ! Sahar");
